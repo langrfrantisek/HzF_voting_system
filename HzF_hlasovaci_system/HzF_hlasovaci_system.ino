ada8752ab0b7c0655fd47419582780a6f7cabe0c
Frantisek <langrfrantisek@gmail.com>
2021-08-06 18:36:51 +0200

    neopixel and display react to pushed btn

--- HzF_hlasovaci_system/HzF_hlasovaci_system.ino
+++ HzF_hlasovaci_system/HzF_hlasovaci_system.ino
@@ -20,7 +20,6 @@ CRGB leds[NUM_LEDS];
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
 const int chipSelect = 4;   // pin where SD card module CS pin is connected
-int i = 0;                  // testing variable
 
 const int ADCpin = A7;      // input pin for button - resistor ladder
 int ADCvalue = 0;           // variable to store the value coming from button - resistor ladder
@@ -28,6 +27,13 @@ int ADCvalue = 0;           // variable to store the value coming from button - 
 const int logo_sw_pin = 2;  // input pin for LOGO switch
 bool logo_sw_value = 0;     // variable to store LOGO switch state
 
+// variable to store band votes
+uint8_t first_band_votes = 0;  
+uint8_t second_band_votes = 0;
+uint8_t third_band_votes = 0;
+uint8_t fourth_band_votes = 0;
+uint8_t fifth_band_votes = 0;
+uint8_t sixth_band_votes = 0;
 
 /************************************************************************************************/
 void setup() {
@@ -67,6 +73,8 @@ ISR(TIMER2_OVF_vect)
   static uint8_t number_of_overflows = 0;
   number_of_overflows++;
 
+  static bool btn_pressed = 0;
+
   if (number_of_overflows == 1)
   {
     /*logo_sw_value = digitalRead(logo_sw_pin);   // store LOGO switch value
@@ -75,10 +83,13 @@ ISR(TIMER2_OVF_vect)
       else lcd.print("off");*/
 
     ADCvalue = analogRead(ADCpin);              // store ADC value
-    delay(100);
-
-    leds[0] = CRGB ( 0, 255, 0);
-    FastLED.show();
+//    delay(100);
+    for ( int i = 0; i<=5; i++)
+    {
+      leds[i] = CRGB ( 0, 0, 0);
+      FastLED.show();  
+    }
+    
 
     if (ADCvalue >= 900)
     {
@@ -89,38 +100,63 @@ ISR(TIMER2_OVF_vect)
     {
       lcd.setCursor(0, 1);                        // set cursor on first position and second line
       lcd.print("BTN: 1  ");
+      leds[5] = CRGB ( 0, 255, 0);
+      FastLED.show();
+      btn_pressed = 1;
     }
     else if (ADCvalue >= 70 && ADCvalue <= 120)
     {
       lcd.setCursor(0, 1);                        // set cursor on first position and second line
       lcd.print("BTN: 2  ");
+      leds[4] = CRGB ( 0, 255, 0);
+      FastLED.show();
+      btn_pressed = 1;
     }
     else if (ADCvalue >= 200 && ADCvalue <= 270)
     {
       lcd.setCursor(0, 1);                        // set cursor on first position and second line
       lcd.print("BTN: 3  ");
+      leds[3] = CRGB ( 0, 255, 0);
+      FastLED.show();
+      btn_pressed = 1;
     }
     else if (ADCvalue >= 420 && ADCvalue <= 470)
     {
       lcd.setCursor(0, 1);                        // set cursor on first position and second line
       lcd.print("BTN: 4  ");
+      leds[2] = CRGB ( 0, 255, 0);
+      FastLED.show();
+      btn_pressed = 1;
     }
     else if (ADCvalue >= 580 && ADCvalue <= 630)
     {
       lcd.setCursor(0, 1);                        // set cursor on first position and second line
       lcd.print("BTN: 5  ");
+      leds[1] = CRGB ( 0, 255, 0);
+      FastLED.show();
+      btn_pressed = 1;
     }
     else if (ADCvalue >= 690 && ADCvalue <= 750)
     {
       lcd.setCursor(0, 1);                        // set cursor on first position and second line
       lcd.print("BTN: 6  ");
+      leds[0] = CRGB ( 0, 255, 0);
+      FastLED.show();
+      btn_pressed = 1;
     }
     else
     {
       lcd.setCursor(0, 1);                        // set cursor on first position and second line
       lcd.print("ERROR   ");
+      btn_pressed = 1;
     }
-  } else if (number_of_overflows == 10) number_of_overflows = 0;
+  } 
+  else if (number_of_overflows == 2 && btn_pressed == 0) number_of_overflows = 0;
+  else if (number_of_overflows == 100 && btn_pressed == 1) 
+  {
+    number_of_overflows = 0;
+    btn_pressed = 0;
+  }
 
   //  if (number_of_overflows == 5)
   //  {

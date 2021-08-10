7f2c3bd0c84c16e7016203a49a013895c1eb4cbc
Frantisek <langrfrantisek@gmail.com>
2021-08-10 22:09:15 +0200

    comments added

--- HzF_hlasovaci_system/HzF_hlasovaci_system.ino
+++ HzF_hlasovaci_system/HzF_hlasovaci_system.ino
@@ -13,10 +13,12 @@
 #include <LiquidCrystal_I2C.h>  // library for LCD
 #include <SD.h>                 // library for SD card module
 #include <FastLED.h>            // library for Neopixel adressable RGB strip WS2812
-#define LED_PIN     7
-#define NUM_LEDS    18
+#define LED_PIN     7           // pin where NEOPIXEL data line is connected
+#define NUM_LEDS    18          // number of LEDs on LED strip
 CRGB leds[NUM_LEDS];
-uint8_t const columns = 11;
+uint8_t const columns = 11;     //number of columns of colors 2D array
+// 2D array storing logo animation colors (flickering if nuber lower than maximum(255) is used)
+// first 10 columns for logo circle, last one for center LED
 uint8_t colors[3][columns] = {
   {   0,   0,   0,   0,   0, 255, 255, 255, 255, 255, 255},
   {   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, 255},
@@ -50,7 +52,8 @@ void setup() {
   // LCD settings
   lcd.begin(16, 2);             // LCD initialization
   lcd.clear();
-  lcd.setCursor(0, 0);
+  // default display view
+  lcd.setCursor(0, 0);          // lcd.setCursor(postion, line); indexing from 0
   lcd.print("    ^^    ^^    ");
   lcd.setCursor(0, 1);
   lcd.print("    ^^    ^^    ");
@@ -65,7 +68,7 @@ void setup() {
   cli();                        //stop interrupts for till we make the settings
   TCCR2A = 0;                   // Reset entire TCCR1A to 0
   TCCR2B = 0;                   // Reset entire TCCR1B to 0
-  TCCR2B |= B00000111;          //  prescaler 1024 -> 16ms
+  TCCR2B |= B00000111;          // prescaler 1024 -> 16ms
   TIMSK2 |= (1 << TOIE2);       // overflow interrupt on Timer1 enable
   sei();                        // Enable interrupts
 
@@ -79,7 +82,7 @@ void loop() {
 /* Interrupt service routines ----------------------------------------*/
 ISR(TIMER2_OVF_vect)
 {
-  TCNT2  = 0;                  //First, set the timer back to 0 so it resets for next interrupt
+  TCNT2  = 0;   //First, set the timer back to 0 so it resets for next interrupt
 
   static uint8_t number_of_overflows = 0;
   number_of_overflows++;
@@ -90,21 +93,21 @@ ISR(TIMER2_OVF_vect)
   if (number_of_overflows == 1)
   {
     logo_sw_value = digitalRead(logo_sw_pin);   // store LOGO switch value
-    button_search();
+    button_search();                            // search if any band button is pressed
   }
   // sets how often will ADC tries to recognize pressed button
   else if (number_of_overflows == 2 && btn_pressed == 0) number_of_overflows = 0;
   // sets how long will ADC stop looking for button pressed after one is pressed
   else if (number_of_overflows == 100 && btn_pressed == 1)
   {
-    number_of_overflows = 0;
-    btn_pressed = 0;
+    number_of_overflows = 0;    // start counting number of overflows again
+    btn_pressed = 0;            // ==0 until band button is pressed
   }
 }
 /* Custom functions --------------------------------------------------*/
+// update number of votes on display (called by button_search();)
 void display_update() {
   /*
-        //lcd.setCursor(postion, line);
         lcd.setCursor(0, 0);
         lcd.print("1111^^2222^^3333");
         lcd.setCursor(0, 1);
@@ -124,12 +127,13 @@ void display_update() {
   lcd.setCursor(12, 1);
   lcd.print(sixth_band_votes);
 
-  sd_update();
+  sd_update();  // store new number of votes on microSD card
 }
-
+// store new number of votes on microSD card (called by display_update();)
 void sd_update() {
-
+  // if microSD card available open hlasy.txt (file created if not there)
   File soubor = SD.open("hlasy.txt", FILE_WRITE); //_ can not be in file name
+  // if file opened successfully write number of actual votes in format 1111:2222:3333:4444:5555:6666
   if (soubor)
   {
     soubor.print(first_band_votes);
@@ -145,7 +149,10 @@ void sd_update() {
     soubor.println(sixth_band_votes);
     soubor.close();
 
-  } else {
+  } 
+  //if microSD is't available print SDerror on display and button LEDs lights red
+  else 
+  {
     lcd.setCursor(0, 0);
     lcd.print("SDerror");
 
@@ -157,7 +164,10 @@ void sd_update() {
   }
 
 }
-
+/* read and store ADCvalue(one wire connection to buttons/resistors ladder) 
+ * define if/which button is pressed, count votes and calls display_update() 
+ * (called by ISR(TIMER2_OVF_vect);)
+ */
 void button_search() {
 
   ADCvalue = analogRead(ADCpin);              // store ADC value
@@ -233,11 +243,12 @@ void button_search() {
     btn_pressed = 1;
   }
 }
-
+// make animation change - by one position shift color of all circle LEDs (called by ISR(TIMER2_OVF_vect);)
 void logo(bool sw)
 {
-  static uint8_t led_num = 6;
-  static uint8_t slow_down = 0;
+  static uint8_t led_num = 6;   // variable for adress of writed LED 
+  static uint8_t slow_down = 0; // counting how many times was functon called
+  // slow down animation by ignoring odd cycles
   if (slow_down % 2 == 0)
   {
     if (sw == 0)  // if logo switch is on
